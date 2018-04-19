20039afe7ff2efa10756d800be83851168d70918
Luong Cong Dan <>
2018-04-19 11:19:41 +0700

    * build only for React not compat with React-Native + testing material-ui

--- App.js
+++ App.js
@@ -1,65 +1,18 @@
 import React from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-  ActivityIndicator
-} from 'react-native';
+import { Provider as StoreProvider } from 'react-redux';
+import HomeScreen from './src/components/HomeScreen';
+import createStore from './src/createStore';
 
-import MoviesList from './src/MoviesList';
-import * as API from './src/api/API';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isLoading: true,
-      dataSource: {}
-    };
-  }
+const store = createStore();
 
-  componentDidMount() {
-    API.getMoviesHome()
-    .then((json) => {
-      // console.log('getMoviesHome ' + JSON.stringify(json.Movies_Banners, 2, null));
+export default () => (
 
-      this.setState({
-        isLoading: false,
-        dataSource: json.Movies_Banners
-      })
-    });
+    <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+      <HomeScreen />
+    </MuiThemeProvider>
 
-    API.getCategory(false)
-    .then((json) => {
-      console.log('getCategory ' + JSON.stringify(json, 2, null));
-    });
-
-    API.getCategoryDetail(75, 0, 3)
-    .then((json) => {
-      console.log('getCategoryDetail ' + JSON.stringify(json, 2, null));
-    })
-  }
-
-  render() {
-    if (this.state.isLoading) {
-      return (
-        <View style={styles.container}>
-          <ActivityIndicator />
-        </View>
-      );
-    }
-
-    return (
-      <MoviesList movies={this.state.dataSource} />
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+);
