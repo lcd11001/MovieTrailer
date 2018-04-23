c88ca8841b1cafc66f7839f4eab4e8ed0307f4b9
Luong Cong Dan <>
2018-04-23 17:07:05 +0700

    - scrollable tabs

--- src/LearningMaterialUI/Components/Layouts/Footer.js
+++ src/LearningMaterialUI/Components/Layouts/Footer.js
@@ -4,33 +4,33 @@ import Tabs, { Tab } from 'material-ui/Tabs'
 import withWidth from 'material-ui/utils/withWidth'
 
 export default withWidth()(
-  ({ muscles, category, onSelect }) => {
-    const index = category
-      ? muscles.findIndex(group => group === category) + 1
-      : 0
+    ({ muscles, category, onSelect, width }) => {
+        const index = category
+            ? muscles.findIndex(group => group === category) + 1
+            : 0
 
-    const onIndexChange = (event, value) => {
-      onSelect(value === 0 ? '' : muscles[value - 1])
+        const onIndexChange = (event, value) => {
+            onSelect(value === 0 ? '' : muscles[value - 1])
+        }
+
+        return (
+            <Paper>
+                <Tabs
+                    value={index}
+                    onChange={onIndexChange}
+                    indicatorColor='primary'
+                    textColor='primary'
+                    centered={width !== 'xs'}
+                    scrollable={width === 'xs'}
+                >
+                    <Tab label='all' key={0} />
+                    {
+                        muscles.map((group, i) => (
+                            <Tab label={group} key={i + 1} />
+                        ))
+                    }
+                </Tabs>
+            </Paper>
+        )
     }
-
-    return (
-      <Paper>
-        <Tabs
-          value={index}
-          onChange={onIndexChange}
-          indicatorColor='primary'
-          textColor='primary'
-          scrollable
-          scrollButtons="auto"
-        >
-          <Tab label='all' key={0} />
-          {
-            muscles.map((group, i) => (
-              <Tab label={group} key={i + 1} />
-            ))
-          }
-        </Tabs>
-      </Paper>
-    )
-  }
 )
