ee2ca429c6797860b18f32a8d1c0146dc3fa3088
Luong Cong Dan <>
2018-05-23 00:26:50 +0700

    * tabs styles

--- src/MovieTrailer/components/IconLabelTabs.js
+++ src/MovieTrailer/components/IconLabelTabs.js
@@ -1,38 +1,42 @@
-import React, { Fragment } from 'react';
-import PropTypes from 'prop-types';
+import React, { Fragment } from 'react'
+import PropTypes from 'prop-types'
+
+import compose from 'recompose/compose'
+import withWidth from '@material-ui/core/withWidth'
 import { withStyles } from '@material-ui/core/styles'
 
-import Paper from '@material-ui/core/Paper';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper'
+import Tabs from '@material-ui/core/Tabs'
+import Tab from '@material-ui/core/Tab'
+import Typography from '@material-ui/core/Typography'
 
 import { tabsStyles as styles } from '../styles'
 
 function TabContainer(props) {
+    console.log('TabContainer', props)
     return (
         <Typography component="p" className={props.classes.content}>
-            <span className={props.classes.firstLetter}>
+            <span className={`${props.classes.firstLetter}  ${props.classes.firstLetterPadding}`}>
                 {props.children.substring(0, 1)}
             </span>
             {props.children.substring(1)}
         </Typography>
-    );
+    )
 }
 
 TabContainer.propTypes = {
     children: PropTypes.node.isRequired,
     classes: PropTypes.object.isRequired,
-};
+}
 
 class IconLabelTabs extends React.Component {
     state = {
         value: 0,
-    };
+    }
 
     handleChange = (event, value) => {
-        this.setState({ value });
-    };
+        this.setState({ value })
+    }
 
     render() {
         const {
@@ -58,7 +62,7 @@ class IconLabelTabs extends React.Component {
                             data.map((value, index) => (
                                 <Tab key={index} 
                                     icon={
-                                        <img src={value.icon} alt={value.icon} />
+                                        <img src={value.icon} alt={value.icon} className={classes.tabIcon} />
                                     } 
                                     label={
                                         <Typography className={classes.tabLabel}>{value.label}</Typography>
@@ -73,7 +77,7 @@ class IconLabelTabs extends React.Component {
 
                 <TabContainer {...{classes: this.props.classes}}>{data[value].content}</TabContainer>
             </Fragment>
-        );
+        )
     }
 }
 
@@ -81,4 +85,4 @@ IconLabelTabs.propTypes = {
     classes: PropTypes.object.isRequired,
 }
 
-export default withStyles(styles)(IconLabelTabs)
+export default compose ( withStyles(styles), withWidth() )(IconLabelTabs)
--- src/MovieTrailer/styles/tabsStyles.js
+++ src/MovieTrailer/styles/tabsStyles.js
@@ -3,21 +3,78 @@ import textStyles from './textStyles'
 
 const styles = theme => ({
     content: {
-        ...textStyles(theme, {textColor: theme.palette.text.primary, textSize: 1.7, textDecreaseSize: 0.4, textWrap: 'normal'}),
+        ...textStyles(theme, {textColor: theme.palette.text.primary, textSize: 1.7, textDecreaseSize: 0.25, textWrap: 'normal'}),
         margin: 30
     },
     firstLetter: {
-        ...textStyles(theme, {textColor: theme.palette.primary.main, textSize: 5.0, textDecreaseSize: 1.0}),
+        ...textStyles(theme, {textColor: theme.palette.primary.main, textSize: 5.0, textDecreaseSize: 0.8}),
+    },
+    firstLetterPadding: {
         float: 'left',
         paddingTop: 20,
         paddingRight: 8,
-        paddingLeft: 3
+        paddingLeft: 3,
+        // extra small
+        [theme.breakpoints.only('xs')]: {
+            paddingTop: 8,
+            paddingRight: 4,
+        },
+        // small
+        [theme.breakpoints.only('sm')]: {
+            paddingTop: 11,
+            paddingRight: 5,
+        },
+        // medium
+        [theme.breakpoints.only('md')]: {
+            paddingTop: 14,
+            paddingRight: 6,
+        },
+        // large
+        [theme.breakpoints.only('lg')]: {
+            paddingTop: 17,
+            paddingRight: 7,
+        }
     },
     tabLabel: {
         ...textStyles(theme, {textColor: theme.palette.text.secondary, textSize: 1.3, textDecreaseSize: 0.2}),
     },
     tab: {
-        height: 100
+        height: 100,
+        // extra small
+        [theme.breakpoints.only('xs')]: {
+            height: 60,
+        },
+        // small
+        [theme.breakpoints.only('sm')]: {
+            height: 70,
+        },
+        // medium
+        [theme.breakpoints.only('md')]: {
+            height: 80,
+        },
+        // large
+        [theme.breakpoints.only('lg')]: {
+            height: 90,
+        }
+    },
+    tabIcon: {
+        zoom: 1.0,
+        // extra small
+        [theme.breakpoints.only('xs')]: {
+            zoom: 0.6,
+        },
+        // small
+        [theme.breakpoints.only('sm')]: {
+            zoom: 0.7,
+        },
+        // medium
+        [theme.breakpoints.only('md')]: {
+            zoom: 0.8,
+        },
+        // large
+        [theme.breakpoints.only('lg')]: {
+            zoom: 0.9,
+        }
     }
 })
 
