a9cd6ad35f31aa95e87c91dfde0ed023ad7176e2
Luong Cong Dan <lcd11001@gmail.com>
2018-11-30 11:32:52 +0700

    - edit src

--- src/MovieTrailer/App.js
+++ src/MovieTrailer/App.js
@@ -1,6 +1,5 @@
-import React, { Component } from 'react'
-// import { BrowserRouter as Router } from 'react-router-dom'
-import { Switch, Route } from 'react-router-dom'
+import React, { Component, Fragment } from 'react'
+import { Switch, Route, Redirect } from 'react-router-dom'
 // import Home from './containers/Home'
 import MovieDetail from './containers/MovieDetail'
 import PlayMovie from './containers/PlayMovie'
@@ -10,28 +9,40 @@ import Home from './drawer_containers/Home'
 import { NotFound } from './components/errors'
 
 export default class App extends Component {
-    _renderHome = (props) => (
-        <Home {...props} />
-    )
+    _renderHome = (props) => {
+        const { classes, ...other } = props
+        return (
+            <Home {...other} />
+        )
+    }
 
-    _renderMovieDetail = (props) => (
-        <MovieDetail {...props} />
-    )
+    _renderMovieDetail = (props) => {
+        const { classes, ...other } = props
+        return (
+            <MovieDetail {...other} />
+        )
+    }
 
-    _renderPlayMovie = (props) => (
-        <PlayMovie {...props} />
-    )
+    _renderPlayMovie = (props) => {
+        const { classes, ...other } = props
+        return (
+            <PlayMovie {...other} />
+        )
+    }
 
     render() {
         return (
-            <Switch>
-                <Route exact path='/' render={this._renderHome} />
-                <Route exact path={`/movie/:movieID`} render={this._renderMovieDetail} />
-                <Route exact path={`/play/:movieID`} render={this._renderPlayMovie} />
-                <Route exact path={`/trailer/:url/:movieID`} render={this._renderPlayMovie} />
+            <Fragment>
+                <Switch>
+                    <Route exact path='/' render={() => <Redirect to='/home' />} />
+                    <Route exact path='/home' render={this._renderHome} />
+                    <Route exact path={`/movie/:movieID`} render={this._renderMovieDetail} />
+                    <Route exact path={`/play/:movieID`} render={this._renderPlayMovie} />
+                    <Route exact path={`/trailer/:url/:movieID`} render={this._renderPlayMovie} />
 
-                <Route component={NotFound} />
-            </Switch>
+                    <Route component={NotFound} />
+                </Switch>
+            </Fragment>
         )
     }
 }
--- src/index.js
+++ src/index.js
@@ -10,13 +10,30 @@ import { Provider } from 'react-redux'
 // https://github.com/ReactTraining/react-router/tree/master/packages/react-router-redux
 import { ConnectedRouter } from 'react-router-redux'
 import { store, history } from './MovieTrailer/redux/store'
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+
 import App from './MovieTrailer/App'
 
+// https://material-ui.com/customization/default-theme/
+const theme = createMuiTheme({
+    palette: {
+        background: {
+            default: '#f2f2f2', // body background-color
+        }
+    }
+})
+
+const RenderApp = () => (
+    <MuiThemeProvider theme={theme}>
+        <Provider store={store}>
+            <ConnectedRouter history={history}>
+                <App />
+            </ConnectedRouter>
+        </Provider>,
+    </MuiThemeProvider>
+)
+
 ReactDOM.render(
-    <Provider store={store}>
-        <ConnectedRouter history={history}>
-            <App />
-        </ConnectedRouter>
-    </Provider>,
+    <RenderApp />,
     document.getElementById('root')
 )
