fbdef12509f54a840137f5de9450124c066b4da9
Luong Cong Dan <lcd11001@gmail.com>
2018-11-30 23:21:23 +0700

    - remove redux logger

--- src/MovieTrailer/redux/store.js
+++ src/MovieTrailer/redux/store.js
@@ -24,7 +24,7 @@ export const store = createStore(
     }),
     {},
     applyMiddleware(
-        createLogger(), 
+        // createLogger(), 
         thunk, 
         promise(),
         routerMiddleware(history)
