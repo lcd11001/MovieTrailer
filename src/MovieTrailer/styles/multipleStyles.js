cabf91b88c1fd6b8b0c34555cfeb574c00dbf297
Luong Cong Dan <lcd11001@gmail.com>
2018-12-05 10:12:37 +0700

    * logic

--- src/MovieTrailer/styles/multipleStyles.js
+++ src/MovieTrailer/styles/multipleStyles.js
@@ -4,7 +4,7 @@ import { withStyles } from '@material-ui/core/styles'
 const withMultipleStyles = (...params) => {
     return withStyles((theme) => {
         var styles = {}
-        for (var len = params.length, styles = new Array(len), key = 0; key < len; key++) {
+        for (var len = params.length, key = 0; key < len; key++) {
             styles = Object.assign(styles, params[key](theme));
         }
 
