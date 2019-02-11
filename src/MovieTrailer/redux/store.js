a1e959d6228e5269585bf8cf0e23880396c4b4be
Luong Cong Dan <lcd11001@gmail.com>
2019-02-11 08:48:29 +0700

    * update version + redux devtools extension

--- src/MovieTrailer/redux/store.js
+++ src/MovieTrailer/redux/store.js
@@ -3,7 +3,8 @@ import {
     applyMiddleware,
     combineReducers
 } from 'redux'
-import { createLogger } from 'redux-logger'
+// import { createLogger } from 'redux-logger'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import thunk from 'redux-thunk'
 import promise from 'redux-promise-middleware'
 
@@ -24,10 +25,12 @@ export const store = createStore(
         router: connectRouter(history),
     }),
     {},
-    applyMiddleware(
-        createLogger(), 
-        thunk, 
-        promise(),
-        routerMiddleware(history)
+    composeWithDevTools(
+        applyMiddleware(
+            // createLogger(), 
+            thunk,
+            promise,
+            routerMiddleware(history)
+        )
     )
 )
